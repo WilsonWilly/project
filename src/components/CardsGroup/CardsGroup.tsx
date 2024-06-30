60f101232e923ce5c60d0b42e029419f44ff47e7
Yoann-Guion <yoo.guion@gmail.com>
2024-06-30 17:00:36 +0200

    map on miniature with fake data

--- src/components/CardsGroup/CardsGroup.tsx
+++ src/components/CardsGroup/CardsGroup.tsx
@@ -1,26 +1,14 @@
 import Miniature from "./Miniature";
 
+import { boardGames } from '../../assets/data' ;
+
 function CardsGroup() {
     return (
         <div className="flex flex-1 flex-wrap justify-around">
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
-            <Miniature />
+            {boardGames.map((boardGame) => (
+                <Miniature key={boardGame.id} name={boardGame.name} description={boardGame.description} picture={boardGame.picture}/>
+            ))}
+
 
         </div>
     )
--- src/components/CardsGroup/Miniature.tsx
+++ src/components/CardsGroup/Miniature.tsx
@@ -1,6 +1,13 @@
 import { Card, Image, CardFooter } from "@nextui-org/react";
 
-function Miniature() {
+interface MiniatureProps {
+    name: string,
+    description: string,
+    picture: string,
+
+}
+
+function Miniature({name, description, picture}: MiniatureProps) {
     return (
         <Card isFooterBlurred fullWidth radius="lg"
         className="m-1 h-32 md:w-60 md:h-60">
@@ -8,12 +15,12 @@ function Miniature() {
                 removeWrapper
                 alt="Boardgame cover"
                 className="z-0 w-full h-full object-cover"
-                src="https://cf.geekdo-images.com/35h9Za_JvMMMtx_92kT0Jg__original/img/jt70jJDZ1y1FWJs4ZQf5FI8APVY=/0x0/filters:format(jpeg)/pic7149798.jpg"
+                src={picture}
             />
             <CardFooter className="absolute z-10 bottom-0 flex-col !items-start bg-white/35 rounded-lg">
                 <p></p>
-                <h3 className="text-medium text-black uppercase font-bold">7 Wonders</h3>
-                <h4 className="text-grey font-light text-xs">You are the leader of one of the 7 great cities...</h4>
+                <h3 className="text-medium text-black uppercase font-bold">{name}</h3>
+                <h4 className="text-grey font-light text-xs">{description}</h4>
             </CardFooter>
         </Card>
     )
