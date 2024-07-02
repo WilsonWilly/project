58de2cf10fd51677d7decdbc2a98963f51fddaa9
WIlsonWilly <wilsonrabbitreseaux@gmail.com>
2024-07-02 09:52:36 +0200

    header edited

--- src/app/apropos/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-export default function AboutPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>HELLO WORLD AboutPage</p>
-    </main>
-  );
-}
